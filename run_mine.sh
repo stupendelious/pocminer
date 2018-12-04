d98ad5ea9cf84405b320995de34673b4af0797b9
stupendelious <45348932+stupendelious@users.noreply.github.com>
2018-12-04 16:17:21 -0600

    Update run_mine.sh

--- run_mine.sh
+++ run_mine.sh
@@ -1,2 +1,2 @@
-java -Xmx750m -cp pocminer.jar:lib/*:lib/akka/*:lib/jetty/* pocminer.POCMiner mine "http://127.0.0.1:8125"
+java -Xmx750m -cp pocminer.jar:lib/*:lib/akka/*:lib/jetty/* pocminer.POCMiner mine "http://127.0.0.1:17003"
 
